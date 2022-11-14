bdcc5208d2b59b0a56f4548bef4b6f5d7c057f2e
faisalalbarkah1414 <faisalalbarkah1414@gmail.com>
2022-11-14 12:52:01 +0700

    Initial Commit

---
+++ CRUD_SwiftUI/CRUD_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_SwiftUIApp.swift
+//  CRUD_SwiftUI
+//
+//  Created by faisalalbarkah on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
